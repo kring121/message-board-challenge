7c1e2608fa83b975a052b5e9fa1bec7f4e935432
Christian von Lehe <kring.vonlehe@gmail.com>
2019-05-17 14:35:28 -0400

    added: mapstatetoprops, passing messages and rendering them

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,27 +1,37 @@
 import React, { Fragment, useEffect} from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getMessages } from '../actions/messages';
 
-const Messages = ({ getMessages }) => {
+const Messages = ({ getMessages, messages: { messages } }) => {
   useEffect(() => {
     getMessages();
   }, [getMessages]);
 
   return (
-    <h1>Posts</h1>
+    <Fragment>
+      <h1>Messages</h1>
+      {messages.map(message => (
+          <div key={message.id}>
+            <h1>{message.title}</h1>
+            <h2>{message.author}</h2>
+          </div>
+        ))
+      }
+    </Fragment>
   );
 };
 
-// Messages.propTypes = {
-//   getMessages: PropTypes.func.isRequired,
-//   message: PropTypes.object.isRequired
-// };
+Messages.propTypes = {
+  getMessages: PropTypes.func.isRequired,
+  messages: PropTypes.object.isRequired
+};
 
-// const mapStateToProps = state => ({
-//   message: state.post
-// });
+const mapStateToProps = state => ({
+  messages: state.messages
+});
 
 export default connect(
-  null,
+  mapStateToProps,
   { getMessages }
 )(Messages);
